84506c89a693d7bfc073e9400c062bc9874e0cb3
Beverly Cutajar <Beverly.Cutajar.c10200@mcast.edu.mt>
2020-11-17 13:13:17 +0100

    1st lesson. creation of contact

---
+++ Solution1/PresentationApp/Controllers/SupportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationApp.Controllers
+{
+    public class SupportController : Controller
+    {
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact (string email, string query)
+        {
+
+            ViewData["feedback"] = "Thanks for your query, we will be with you shortly";
+
+            ViewData["warning"] = "Kindly fill all the fields";
+            
+            return View();
+        }
+    }
+}
